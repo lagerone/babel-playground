6809f52b61419759c761eb705da72755202138b2
Daniel Lager <daniel.lager@tretton37.com>
2015-04-30 10:11:38 +0200

    setup deploy to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,26 @@ var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+	return gulp.src('./dist/**/*')
+		.pipe(ghPages());
+});
+
 gulp.task('modules', function() {
 	return browserify({
-		entries: './src/main.js',
-		debug: true
-	})
-	.transform(babelify)
-	.bundle()
-	.on('error', function(err){
-      console.log(err.message);
-      return this;
-    })
-	.pipe(source('main-built.js'))
-	.pipe(gulp.dest('./dist'));
+			entries: './src/main.js',
+			debug: true
+		})
+		.transform(babelify)
+		.bundle()
+		.on('error', function(err){
+	      console.log(err.message);
+	      return this;
+	    })
+		.pipe(source('main-built.js'))
+		.pipe(gulp.dest('./dist'));
 });
 
 
